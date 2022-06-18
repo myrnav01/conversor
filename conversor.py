f078946ee688d631687eb2c37829246af56009fa
Mirna Veras <mirna.programas@gmail.com>
2022-06-18 15:41:36 -0500

    convirtiendo multiples monedas a dolares

--- conversor.py
+++ conversor.py
@@ -1,7 +1,37 @@
-pesos = input("¿Cuántos pesos dominicanos tienes? ")
-pesos = float(pesos)
-valor_dolar = 55.3
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes $" + dolares + " dólares")
+menu = """
+Bienvenido al conversor de monedas 💰💰💰
+
+1- Pesos dominicanos
+2- Pesos argentinos
+3- Pesos mexicanos
+
+Elige una opción: """
+
+opcion = int(input(menu))
+
+if opcion == 1:
+    pesos = input("¿Cuántos pesos dominicanos tienes? ")
+    pesos = float(pesos)
+    valor_dolar = 55
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+elif opcion == 2:
+    pesos = input("¿Cuántos pesos argentinos tienes? ")
+    pesos = float(pesos)
+    valor_dolar = 65
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+elif opcion == 3:
+    pesos = input("¿Cuántos pesos mexicanos tienes? ")
+    pesos = float(pesos)
+    valor_dolar = 24
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+else:
+    print('Ingresa una opción correcta, por favor.')
